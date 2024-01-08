8dd0240e7988527a8fc12a0e7e6bf1ce39a6b403
unknown <Roberio Matos>
2024-01-07 21:26:50 -0300

    update validationsHelper tests

--- src/helpers/__tests__/validationsHelper.test.ts
+++ src/helpers/__tests__/validationsHelper.test.ts
@@ -1,4 +1,4 @@
-import { validateEmail } from '..';
+import { validateEmail, validatePassword } from '..';
 
 describe('validationsHelper.ts > validateEmail', () => {
   test('should return the expected values', () => {
@@ -7,6 +7,18 @@ describe('validationsHelper.ts > validateEmail', () => {
     expect(isValid1).toBe(false);
 
     const isValid2 = validateEmail('test@test.com');
+
+    expect(isValid2).toBe(true);
+  });
+});
+
+describe('validationsHelper.ts > validatePassword', () => {
+  test('should return the expected values', () => {
+    const isValid1 = validatePassword('test');
+
+    expect(isValid1).toBe(false);
+
+    const isValid2 = validatePassword('123456');
 
     expect(isValid2).toBe(true);
   });
